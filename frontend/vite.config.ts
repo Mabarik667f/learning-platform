196bc46e85f197fd154035af6dc5009958a4ed09
Mabarik667f <jc.bob1412@gmail.com>
2024-08-14 02:17:02 +0300

    settings: update ts | vite configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from "url";
+
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const host: string = process.env.VITE_HOST || "127.0.0.1";
+const port: string = process.env.VITE_PORT || "8000";
+const protocol: string = process.env.VITE_PROTOCOL || "http";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +17,16 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/v1": {
+        target: `${protocol}://${host}:${port}`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/v1/, ""),
+      },
     },
   },
 });
